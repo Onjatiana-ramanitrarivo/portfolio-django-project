b67e18329c8d8336d393bea097dba23e898b8dcf
onjatiana ramanitrarivo <onjatiana.ramanitrarivo@gmail.com>
2023-12-08 09:41:15 +0300

    Mise en place de la structure de base du site 'presentation'

---
+++ presentation/url.py
@@ -0,0 +1,6 @@
+from presentation import views
+from django.urls import path
+
+urlpatterns = [
+    path('presentation/',views.accueil)
+]
--- presentation/views.py
+++ presentation/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+def accueil(request):
+
+    template = loader.get_template('index.html')
+    context = {
+        "title":"homepage",
+    }
+
+    return HttpResponse(template.render(context,request))
