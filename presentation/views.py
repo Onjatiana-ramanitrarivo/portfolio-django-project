b531d73917e8cc0d1eca96ae387d01abfe7244bf
onjatiana ramanitrarivo <onjatiana.ramanitrarivo@gmail.com>
2023-12-10 10:51:17 +0300

    Envoie d'email réussi

--- presentation/views.py
+++ presentation/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.core.mail import send_mail
 
 def accueil(request):
 
@@ -12,6 +13,32 @@ def accueil(request):
     return HttpResponse(template.render(context,request))
 
 def contact(request):
+
+
+    if request.method == 'POST':
+        fullname = request.POST.get('fullname')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+
+        data = {
+            'fullname':fullname,
+            'email':email,
+            'subject':subject,
+            'message':message
+        }
+
+        message = '''
+        New message : {}
+
+        From: {}
+
+        '''.format(data['message'],data['email'])
+
+        send_mail(data['subject'],message,'',['rama14jill@gmail.com'])
+
+
+
     template = loader.get_template('contact.html')
     context = {
         "title":"contact",
